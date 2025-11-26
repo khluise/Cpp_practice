bfc9417c93fab479384218b1125a56a64c0db2a2
khluise <luisekh076@gmail.com>
2025-11-27 00:28:05 +0600

    added pow, sqrt, abs, ceil, floor inside example_mathOperations.cpp

--- example_mathOperations.cpp
+++ example_mathOperations.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
-//#include <math>
+#include <cmath>
 
 int main()
 {
-    double x = 7;
+    double x = 7.777;
     double y = 4;
     double max, min;
-    
+    double z;
+
     max = std::max(x,y);
     min = std::min(x,y);
 
-    std::cout << max;
-    std::cout << min;
+    std::cout << max << '\n';
+    std::cout << min << '\n';
+    
+    z = pow(x,y);
+    std::cout << z << '\n';
 
+    z = sqrt(x);
+    std::cout << z << '\n';
+
+    z = round(x);
+    std::cout << z << '\n';
+    
+    z = ceil(x);
+    std::cout << z << '\n';
+    
     return 0;
 }
