6a2248e08b4d5b3b36d447eb36a8c0dc23d03349
khluise <luisekh076@gmail.com>
2025-11-26 18:53:01 +0600

    changes example_constant.cpp file to use namespace std

--- example_constant.cpp
+++ example_constant.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
+    using namespace std;
+
     const double PI =  3.14159;
     double radius = 5;
     double height = 10;
@@ -8,9 +10,9 @@ int main() {
     double volumeForCylinder = PI * radius * radius * height;
     double volumeForCone = volumeForCylinder / 3.;
 
-    std::cout << "The volume of a CYLINDER with radius=" << radius << \
+    cout << "The volume of a CYLINDER with radius=" << radius << \
         " height=" << height << " is equal: " << volumeForCylinder << '\n';
-    std::cout << "The volume of a CONE with radius=" << radius << \
+    cout << "The volume of a CONE with radius=" << radius << \
         " height=" << height << " is equal: " << volumeForCone << '\n'; 
 
     return 0;
