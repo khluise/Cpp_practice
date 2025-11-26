b7fd7765b171eb9b47d5fe2898c164f452a2bf88
khluise <luisekh076@gmail.com>
2025-11-26 19:07:34 +0600

    changes the long data type. used text_ instead of std::string

--- example_typedef.cpp
+++ example_typedef.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <vector>
 
-typedef std::vector<std::pair<std::string, int>> pairlist_t;
+//typedef std::vector<std::pair<std::string, int>> pairlist_t;
+typedef std::string text_t;
 
 int main() {
 
-    pairlist_t pairlist;
+    text_t firstName = "Kamrul";
+    
+    std::cout << firstName << '\n';
     
     return 0;
 }
