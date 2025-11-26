5f05ddb519e4bb408dbf11f1a59cb617af760f79
khluise <luisekh076@gmail.com>
2025-11-26 20:27:38 +0600

    Added example_userInput.cpp

---
+++ example_userInput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+    std::string name;
+    int age;
+
+    std::cout << "What's your name?: ";
+    std::cin >> name;
+    
+    std::cout << "What's your age?: ";
+    std::cin >> age;
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << " years old" << '\n';
+
+    return 0;
+}
